e308472d36a1a416f37b04722940efe5def7d5ea
Michael Scribellito <mscribellito@users.noreply.github.com>
2018-01-16 15:09:08 -0500

    Add files via upload

--- FixedWidth/Formatters/BooleanFormatter.cs
+++ FixedWidth/Formatters/BooleanFormatter.cs
@@ -1,1 +1,31 @@
+﻿using System;
 
+namespace FixedWidth.Formatters
+{
+
+    public class BooleanFormatter : ITextFormatter
+    {
+
+        public object Deserialize(string str)
+        {
+
+            if (int.TryParse(str, out int value))
+            {
+                return value == 1 ? true : false;
+            }
+
+            throw new Exception(string.Format("Error converting {0} to boolean",
+                str));
+
+        }
+
+        public string Serialize(object obj)
+        {
+
+            return ((bool)obj) ? "1" : "0";
+
+        }
+
+    }
+
+}
