12234bc893d903d326a71f143f1e7b04793e859d
Michael Scribellito <mscribellito@users.noreply.github.com>
2018-01-16 15:07:39 -0500

    Add files via upload

---
+++ FixedWidthTests/BuiltInTypeTests.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FixedWidth.Formatters;
+
+namespace FixedWidth.Tests
+{
+
+    [TestClass()]
+    public class BuiltInTypeTests
+    {
+
+        [TextSerializable]
+        public class SimpleChar
+        {
+            [TextField(1, 1)]
+            public char Value { get; set; }
+        }
+
+        [TextSerializable]
+        public class SimpleString
+        {
+            [TextField(1, 10)]
+            public string Value { get; set; }
+        }
+
+        [TextSerializable]
+        public class SimpleInt
+        {
+            [TextField(1, 10)]
+            public int Value { get; set; }
+        }
+
+        [TextSerializable]
+        public class SimpleDouble
+        {
+            [TextField(1, 10)]
+            public double Value { get; set; }
+        }
+
+        [TextSerializable]
+        public class SimpleBool
+        {
+            [TextField(1, 1, FormatterType = typeof(BooleanFormatter))]
+            public bool Value { get; set; }
+        }
+
+        [TestMethod()]
+        public void Char_Success()
+        {
+
+            string str = "a";
+
+            var serializer = new TextSerializer<SimpleChar>();
+            var deserialized = serializer.Deserialize(str);
+
+            Assert.AreEqual('a', deserialized.Value);
+            Assert.AreEqual(str, serializer.Serialize(deserialized));
+
+        }
+
+        [TestMethod()]
+        public void String_Success()
+        {
+
+            string str = "asdf      ";
+
+            var serializer = new TextSerializer<SimpleString>();
+            var deserialized = serializer.Deserialize(str);
+
+            Assert.AreEqual("asdf", deserialized.Value);
+            Assert.AreEqual(str, serializer.Serialize(deserialized));
+
+        }
+
+        [TestMethod()]
+        public void Int_Success()
+        {
+
+            string str = "1234      ";
+
+            var serializer = new TextSerializer<SimpleInt>();
+            var deserialized = serializer.Deserialize(str);
+
+            Assert.AreEqual(1234, deserialized.Value);
+            Assert.AreEqual(str, serializer.Serialize(deserialized));
+
+        }
+
+        [TestMethod()]
+        public void Double_Success()
+        {
+
+            string str = "12.3      ";
+
+            var serializer = new TextSerializer<SimpleDouble>();
+            var deserialized = serializer.Deserialize(str);
+
+            Assert.AreEqual(12.3, deserialized.Value);
+            Assert.AreEqual(str, serializer.Serialize(deserialized));
+
+        }
+
+        [TestMethod()]
+        public void Bool_Success()
+        {
+
+            string str = "1";
+
+            var serializer = new TextSerializer<SimpleBool>();
+            var deserialized = serializer.Deserialize(str);
+
+            Assert.AreEqual(true, deserialized.Value);
+            Assert.AreEqual(str, serializer.Serialize(deserialized));
+
+        }
+
+    }
+
+}
---
+++ FixedWidthTests/InstantiateTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FixedWidth.Tests
+{
+
+    [TestClass()]
+    public class InstantiateTests
+    {
+
+        [TextSerializable]
+        public struct StructSuccess
+        {
+        }
+
+        public struct StructFailure
+        {
+        }
+
+        [TextSerializable]
+        public class ClassSuccess
+        {
+        }
+
+        public class ClassFailure
+        {
+        }
+
+        [TestMethod()]
+        public void InstantiateWithClass_Success()
+        {
+            var serializer = new TextSerializer<ClassSuccess>();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void InstantiateWithClass_Failure()
+        {
+            var serializer = new TextSerializer<ClassFailure>();
+        }
+
+        [TestMethod()]
+        public void InstantiateWithStruct_Success()
+        {
+            var serializer = new TextSerializer<StructSuccess>();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void InstantiateWithStruct_Failure()
+        {
+            var serializer = new TextSerializer<StructFailure>();
+        }
+
+    }
+
+}
